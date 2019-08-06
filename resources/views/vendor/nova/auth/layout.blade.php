25eeb97417477dd1177274507b442e2e351a83c9
Nivesh Saharan <nivesh@gmail.com>
2019-08-06 18:46:10 +0530

    Adds google tag manager

--- resources/views/vendor/nova/auth/layout.blade.php
+++ resources/views/vendor/nova/auth/layout.blade.php
@@ -17,8 +17,34 @@
             min-width: 0;
         }
     </style>
+
+    @if(config('analytics.gtm.id'))
+    <!-- Google Tag Manager -->
+    <script>(function( w, d, s, l, i ){
+            w[l] = w[l] || [];
+            w[l].push( {
+                'gtm.start':
+                    new Date().getTime(), event: 'gtm.js'
+            } );
+            var f = d.getElementsByTagName( s )[0],
+                j = d.createElement( s ), dl = l != 'dataLayer' ? '&l=' + l : '';
+            j.async = true;
+            j.src =
+                'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+            f.parentNode.insertBefore( j, f );
+        })( window, document, 'script', 'dataLayer', '{{config('analytics.gtm.id')}}' );</script>
+    <!-- End Google Tag Manager -->
+    @endif
 </head>
 <body class="bg-40 text-black h-full">
+    @if(config('analytics.gtm.id'))
+    <!-- Google Tag Manager (noscript) -->
+    <noscript>
+        <iframe src="https://www.googletagmanager.com/ns.html?id={{config('analytics.gtm.id')}}"
+                height="0" width="0" style="display:none;visibility:hidden"></iframe>
+    </noscript>
+    <!-- End Google Tag Manager (noscript) -->
+    @endif
     <div class="h-full">
         <div class="px-view py-view mx-auto">
             @yield('content')
